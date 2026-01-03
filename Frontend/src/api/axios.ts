51a2ec12aefa162cbada93cdda7284cdaee8be39
Atharva Butte <atharvabutte03@gmail.com>
2026-01-04 01:33:34 +0530

    Force replace favicon

--- Frontend/src/api/axios.ts
+++ Frontend/src/api/axios.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
 import {getAccessToken} from './authtoken'
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: API_BASE_URL,
     headers: {
         "Content-Type" : "application/json"
     }
